82ecff590064947bf3c4ef142c3c27fdb4b135d7
Lorenzo Ramponi <lorenzoramponi18@gmail.com>
2019-03-20 18:25:24 +0100

    Update JsonConnection.php

--- JsonConnection.php
+++ JsonConnection.php
@@ -2,7 +2,7 @@
 
 function JsonConnection($cerca){
 
-$contenuto = file_get_contents("http://www.omdbapi.com/?s=".$cerca."&plot=full&apikey=20ef005e");
+$contenuto = file_get_contents("http://www.omdbapi.com/?s=".$cerca."&plot=full&apikey=PUT YOUR KEY HERE");
 $json = json_decode($contenuto, true);
 return $json;
 
